1dda9f6423aaf20b5d21ca37d176d52489f50392
Elvis Jesus <cristomiroca2502@gmail.com>
2025-04-08 20:26:38 -0400

    FixBug: validación al momento de crear una categoria

--- TEST_EH_MVC/DTOs/CategoriasDTO.cs
+++ TEST_EH_MVC/DTOs/CategoriasDTO.cs
@@ -1,12 +1,17 @@
-﻿namespace TEST_EH_MVC.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TEST_EH_MVC.DTOs
 {
     public class CategoriasDTO
     {
         public int CatID { get; set; } = new();
         public int ProID { get; set; } = new();
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [StringLength(25, ErrorMessage = "El nombre no puede tener más de 25 caracteres.")]
         public string? CatNombre { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
         public string? CatDescripcion { get; set; } = string.Empty;
-        public List<CategoriasDTO> Categorias = new();
+        public List<CategoriasDTO> Categorias { get; set; } = new();
 
     }
 }
